c07de45442ac18a63f9ddf1f3a7bf9746c39752f
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-02-26 13:28:09 +0000

    initial variables done

---
+++ Basics_JavaScript/variables.js
@@ -0,0 +1,9 @@
+const customerId = 11556644
+/*Two Ways To Define Variables in Js const && let*/
+let email = 'example@gmail.com'
+var someInfo = 'SomeInfo' 
+//Don't use Var Due to Scope Related Issues
+someData= "someData"
+let noData;
+
+console.table([customerId,email,someInfo,someData, noData]);
