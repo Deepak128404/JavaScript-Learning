a9a3046597fd2ac102971be27267161a45698958
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-02-29 16:51:01 +0000

    initial commit of operations class

---
+++ Basics_JavaScript/3_operations.js
@@ -0,0 +1,13 @@
+// let value = 3
+
+// let negValue = -(value)
+// console.log(negValue);
+//same operators like matho +,-,**,*, and basic operators
+
+// let str1 = 'hello'
+// let str2 =' Deepak'
+// console.log(str1+str2) //string concatination
+
+console.log('1'+ 1 + 1); //111
+console.log(1+1+'1'); // 21 
+//this comes from ecma script
