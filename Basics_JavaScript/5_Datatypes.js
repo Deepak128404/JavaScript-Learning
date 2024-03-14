dc56b716d87266c2703513469e42c074c119c876
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-14 11:46:00 +0000

    Data Types Done

---
+++ Basics_JavaScript/5_Datatypes.js
@@ -0,0 +1,24 @@
+let Number =  5555;
+
+let Bool = true;
+
+let String = "hey this is string";
+
+let Server = null;
+
+let Later = undefined;
+
+//symbol & bigint
+console.table([Number, Bool, String, Server, Later]);
+
+//Primitive DataTypes
+
+
+//Non-Primitive DataTypes Function, Array, Objects
+let myFunction = Function();{
+   console.log("Hello From Function");
+}
+
+let myArray = ['1','2','3', '4'];
+
+console.log(myArray);
