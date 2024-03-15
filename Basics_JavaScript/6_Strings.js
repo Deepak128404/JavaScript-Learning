fb3f3777dd03962a13f841d4e98af67c1ce3f0ea
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-15 15:40:52 +0000

    some basic string done

---
+++ Basics_JavaScript/6_Strings.js
@@ -0,0 +1,9 @@
+const oldString = 'First'
+console.log(oldString + 'Letter');
+
+// old way or using with + or , etc etc 
+
+const newString = "Deepak"
+const gitRepo = 'JavaScript-Learning'
+
+console.log(`Hello This is ${newString}, Welcome to my Repo ${gitRepo}`);
