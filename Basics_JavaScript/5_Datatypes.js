cd59f6f343c051c684995ff79fa43bbb87e82991
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-15 07:13:18 +0000

    Added Stack and Heap Memory Example

--- Basics_JavaScript/5_Datatypes.js
+++ Basics_JavaScript/5_Datatypes.js
@@ -22,3 +22,29 @@ let myFunction = Function();{
 let myArray = ['1','2','3', '4'];
 
 console.log(myArray);
+
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+//Stack Memory
+let variableOne =10;
+let variableTwo = variableOne; // in Stack Memory Only a Copy of The Original Data is sent
+
+variableTwo= 20;
+console.log(variableOne);
+console.log(variableTwo);
+
+//Heap Memory
+
+let userOne = {
+   'userEmail': 'userOne@g.com',
+   'userName': 'someuser'
+}
+
+
+let userTwo = userOne; //Reference of Original value is sent Thus changing in userTwo will lead to change in userOne as well
+
+userTwo.userEmail = 'userTwo@g.com';
+
+console.log(userOne.userEmail);
+console.log(userTwo.userEmail);
