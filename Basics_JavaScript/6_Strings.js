875b72f41d2f5f464717d059d674e6af3a6eb1df
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-15 15:59:00 +0000

    basic string methods Done

--- Basics_JavaScript/6_Strings.js
+++ Basics_JavaScript/6_Strings.js
@@ -1,9 +1,25 @@
-const oldString = 'First'
-console.log(oldString + 'Letter');
+// const oldString = 'First'
+// console.log(oldString + 'Letter');
 
-// old way or using with + or , etc etc 
+// // old way or using with + or , etc etc 
 
-const newString = "Deepak"
-const gitRepo = 'JavaScript-Learning'
+// const newString = "Deepak"
+// const gitRepo = 'JavaScript-Learning'
 
-console.log(`Hello This is ${newString}, Welcome to my Repo ${gitRepo}`);
+// console.log(`Hello This is ${newString}, Welcome to my Repo ${gitRepo}`);
+
+const objectString = new String("JavaScript");
+
+// console.log(objectString.charAt(2));
+
+// console.log(objectString.length);
+
+// console.log(objectString.split('Script'));
+
+// console.log(objectString.indexOf("S"));
+
+// console.log(objectString.slice(0, -1));
+
+// console.log(objectString.trimEnd());
+
+
