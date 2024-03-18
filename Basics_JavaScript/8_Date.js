2308be57cf720dd31095af67edcc7a37b9f9a48c
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-18 16:22:43 +0000

    initial date time in js done

---
+++ Basics_JavaScript/8_Date.js
@@ -0,0 +1,19 @@
+const myDate = new  Date()
+
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.toJSON());
+
+console.log(myDate);
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let myCreatedDate = new Date("03-18-2024")
+
+console.log(myCreatedDate.toDateString());
