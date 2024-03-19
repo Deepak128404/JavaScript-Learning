d8a0a72b47828b1fbb3da3e76ab7e16028a3a04d
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-19 17:54:13 +0000

    Day 2 of Arrays Done

---
+++ 2_Basics_Javascript/2_Array.js
@@ -0,0 +1,24 @@
+const marvelHeros = ['Ironman', 'Thor', 'Captain Marvel']
+const dcHeros = ['Batman', 'Superman', 'flash']
+
+// const newArray = marvelHeros.push(dcHeros)
+// console.log(newArray);
+
+// const newArray  = marvelHeros.concat(dcHeros);
+// console.log(newArray);
+
+// const newArray = [...marvelHeros, ...marvelHeros]//spread operator with this we can easily 
+// console.log(newArray);
+
+// const noArray = [1,3,3,4,5,[1,33,44,55,[55,33,55,66,66,[25253,5939,999]]]]; //how to open array ke ander array
+// console.log(noArray.flat(Infinity)); // how many layers to unpack
+
+const str = 'Array';
+
+// console.log(Array.from(str));//Array.from() to convert to array
+
+console.log(Array.of(str));//does similar thing to convert to array 
+
+
+
+
