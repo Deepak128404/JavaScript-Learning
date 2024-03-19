3db735ccfcffbf3d589c602f604f704bbf35955c
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-19 17:14:38 +0000

    array basics day 1 done

---
+++ 2Basics_Javascript/1_Array.js
@@ -0,0 +1,24 @@
+const myArry = [1,2,3,4,5,6,7,8]
+
+// myArry.push(9)
+
+// console.log( myArry);
+
+// myArry.pop()
+
+// console.log(myArry);
+
+// console.log(myArry.includes(5));
+
+// myArry.unshift(25)
+// myArry.shift()
+
+console.log(myArry);
+
+myArry.slice(1,3); //prints the elementes -1
+
+console.log(myArry);//internview questio difference between slice and splice 
+//splice manipulates the original element while slice does not
+myArry.splice(1,3); //deletes the element from the array
+
+console.log(myArry);
