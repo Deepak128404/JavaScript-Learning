00e9baef0781ec27d5223b5dd64c649ba6cb1832
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-22 15:47:02 +0000

    functions basics

--- Functions/1_functions.js
+++ Functions/1_functions.js
@@ -10,3 +10,8 @@ function add(num1 , num2) {
 
 console.log(add(2,5))//function argument
 
+function greetings(Name) {
+    return `welcome to the repo ${Name}`
+}
+
+console.log(greetings('deepak'))
