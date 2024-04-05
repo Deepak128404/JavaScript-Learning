03322086728fab79b23ca2ea9388ccde6ce77473
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-04-05 14:53:13 +0000

    added code to understand scope of a variable

---
+++ Functions/3_scope.js
@@ -0,0 +1,8 @@
+if(true){
+    let first = 'first';
+    const second = 'second';
+    var outsideAvilable = "i'm outside also" //since this variable is available outside it's scope which could lead multiple problems we avoid using var
+}
+
+console.log(outsideAvilable);
+console.log(second);
