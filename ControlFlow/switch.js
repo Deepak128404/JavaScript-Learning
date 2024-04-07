81b3a9a7e550a5adb329b583c44253d98745fb8e
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-04-07 09:14:37 +0000

    added code for switch case

---
+++ ControlFlow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jldll"
+
+switch (month) {
+    case 'jan':
+        console.log("it's january");
+        break;
+    case 'feb':
+        console.log("it's February");
+        break;
+    case 'march':
+        console.log("it's march");
+        break;
+    case 'april':
+        console.log("it's april");
+        break;
+    case 'may':
+        console.log("it's may");
+        break;
+
+    default:
+        console.log('enter a valid month between jan to may');
+        break;
+}
